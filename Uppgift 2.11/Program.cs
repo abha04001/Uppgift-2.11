51b276b20ea9753d8d3c6667ce2c18e80df19137
caspian <casa04001@utb.vaxjo.se>
2022-09-09 15:07:37 +0200

    Add project files.

---
+++ Uppgift 2.11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_2._11
+{   
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hur lång är Elin hoppade sätt i metern?");
+            string x = Console.ReadLine();
+            Console.WriteLine("Hur lång är Elin hoppade sätt i metern?");
+            string y = Console.ReadLine();   
+
+            Console.WriteLine("Elin hoppade " + x + "");
+
+
+        }
+
+
+
+    }
+
+
+}
